f4814361d0ad5e7a150a6ec3feeaa35a58241654
Meehoi <beam_baimon@hotmail.com>
2025-07-04 20:48:34 +0700

    chore: fix map_tiles in build using separate folder

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter(),
+    files: {
+      assets: process.env.NODE_ENV === 'development' ? 'static_dev' : 'static',
+    },
+  },
 };
 
 export default config;
