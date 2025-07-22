baa48657b45fd761bd3cde96c2cd7cb06f4d9114
Meehoi <beam_baimon@hotmail.com>
2025-07-22 21:45:30 +0700

    remove poc leftover

--- src/lib/api/player.ts
+++ src/lib/api/player.ts
@@ -1,11 +1,8 @@
 import { PUBLIC_API_BASE } from '$env/static/public';
 import type { PlayerEventData } from './types';
 
-// Modified type to allow undefined values for players who left
-type PlayerEventDataWithDepartures = Record<string, PlayerEventData[string] | undefined>;
-
 export const getPlayerRealtimePosition = (
-  callback: (data: PlayerEventDataWithDepartures) => void,
+  callback: (data: PlayerEventData) => void,
 ): (() => void) => {
   const evt = new EventSource(`${PUBLIC_API_BASE}/api/player_positions/`);
 
