068cbb1d6097183e85f4dbb3be45f17953357c7f
Meehoi <1791353+beam41@users.noreply.github.com>
2025-09-18 20:54:04 +0700

    update data

--- src/lib/components/Map/PlayerVehicleInfo.svelte
+++ src/lib/components/Map/PlayerVehicleInfo.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import vehiclesKey from '$lib/assets/data/vehicles_key.json';
+  import vehiclesName from '$lib/assets/data/vehicles_name.json';
   import { m as msg } from '$lib/paraglide/messages';
 
   export interface playerVehicleInfoProps {
@@ -12,7 +12,7 @@
 {vehicleKey !== 'None'
   ? msg['map.player_info.driving']({
       vehicle:
-        vehiclesKey[vehicleKey as keyof typeof vehiclesKey] ??
+        vehiclesName[vehicleKey as keyof typeof vehiclesName] ??
         msg['map.player_info.unknown_vehicle'](),
     })
   : msg['map.player_info.on_foot']()}
