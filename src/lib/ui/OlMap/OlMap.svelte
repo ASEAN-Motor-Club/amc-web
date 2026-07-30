a255cb9a4a62f42cb05e54f70ecb05482c1aa98a
Meehoi <1791353+beam41@users.noreply.github.com>
2026-07-30 22:38:13 +0700

    feat: update to 0.7.19

--- src/lib/ui/OlMap/OlMap.svelte
+++ src/lib/ui/OlMap/OlMap.svelte
@@ -103,7 +103,7 @@
 
   const tileLayer = new WebGLTileLayer({
     source: new ImageTile({
-      url: '/map_tiles/718/{z}_{x}_{y}.avif',
+      url: '/map_tiles/719/{z}_{x}_{y}.avif',
       minZoom: 2,
       maxZoom: 5,
       wrapX: false,
